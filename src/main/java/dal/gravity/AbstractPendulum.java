27cb075a19c5eb6d848f066cce700f0d864b62a8
Darius <dr294784@dal.ca>
2017-04-04 12:37:09 -0300

    Refactoring 1 - Abstract Earth Pendulum removed

--- src/main/java/dal/gravity/AbstractPendulum.java
+++ src/main/java/dal/gravity/AbstractPendulum.java
@@ -12,6 +12,7 @@ public abstract class AbstractPendulum {
     private double stringLength, pointMass;
     protected double theta0; 
     protected double g; 
+    public static final double GRAVITY = 9.80665;
 
     /**
      * Creates a new Pendulum instance using
@@ -20,7 +21,7 @@ public abstract class AbstractPendulum {
      * inTheta0: angular displacement at t=0 (0<=theta0)
      * inG: gravitational field value to use
      */
-    public AbstractPendulum (double inLength, double inMass, double inTheta0, double inG) {
+    public AbstractPendulum (double inLength, double inMass, double inTheta0) {
 	if (validStringLength (inLength)) stringLength = inLength;
 	else throw new IllegalArgumentException ("invalid string length: " + inLength);
 	if (validPointMass(inMass)) pointMass = inMass;
@@ -28,11 +29,13 @@ public abstract class AbstractPendulum {
 	if (validDisplacement (inTheta0)) theta0 = inTheta0;
 	else throw new IllegalArgumentException 
 		 ("invalid angular displacement: " + inTheta0);
-	if (validGC (inG)) g = inG;
-	else throw new IllegalArgumentException ("invalid local gravitational field: " + inG);
+	if (validGC (GRAVITY)) g = GRAVITY;
+	else throw new IllegalArgumentException ("invalid local gravitational field: " + GRAVITY);
     }
 
-    private boolean validDisplacement (double val) { return (val >= 0); }
+    
+
+	private boolean validDisplacement (double val) { return (val >= 0); }
     private boolean validPointMass (double val) { return (val > 0); }
     private boolean validStringLength (double val) { return (val > 0); }
     private boolean validGC (double val) { return (val >= 0); }
--- src/main/java/dal/gravity/RegularPendulum.java
+++ src/main/java/dal/gravity/RegularPendulum.java
@@ -3,7 +3,7 @@ package dal.gravity;
 /**
  * Represents a pendulum
  */
-public class RegularPendulum extends AbstractEarthPendulum {
+public class RegularPendulum extends AbstractPendulum {
     private double delta, iterations = 0;
     private double dissipation;
     private double lastTheta, lastVel, lastAccel;
